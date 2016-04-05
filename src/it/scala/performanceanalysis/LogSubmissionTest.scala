3fef1afd3d8d8041138f446ef3f1fb2719ce2b50
Jordi de Vos <devos.jordi@gmail.com>
2016-04-05 13:27:20 +0200

    Code cleanup and refactoring

--- src/it/scala/performanceanalysis/LogSubmissionTest.scala
+++ src/it/scala/performanceanalysis/LogSubmissionTest.scala
@@ -4,13 +4,18 @@ import performanceanalysis.base.IntegrationTestBase
 
 class LogSubmissionTest extends IntegrationTestBase {
 
-  feature("Log Receiver endpoint") {
+  feature("Log Receiver should only support POST operations") {
+    scenario("") {
+      Given("the server is running")
+      When("I do a HTTP GET to '/' on the LogReceiver port")
+      Then("the response should have statuscode 405")
+    }
     scenario("Logs posted at the LogReceiver") {
       // TODO Implement
       Given("the server is running")
       And("""I registered a component with id "parsingConfiguredComponenet" and a metric {"regex" : "+d", "metric-key" : "a-numerical-metric"}""")
       When("""I do a POST with {"logline" : "some action took 101 seconds", "metric-key" : "a-numerical-metric"} to /components/parsConfigComp/logs on the """ +
-          "Administrator port")
+          "LogReceiver port")
       Then("""the response should have statuscode 202""")
     }
   }
