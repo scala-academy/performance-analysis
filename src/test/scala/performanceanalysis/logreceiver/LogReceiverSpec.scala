c9337197fc81d81013a50763f819287674e934df
Ruud Prein <ruud.prein@ing.nl>
2016-04-11 16:32:02 +0200

    #11 Log Receiver should only support POST operations

--- src/test/scala/performanceanalysis/logreceiver/LogReceiverSpec.scala
+++ src/test/scala/performanceanalysis/logreceiver/LogReceiverSpec.scala
@@ -7,5 +7,21 @@ import performanceanalysis.base.SpecBase
   * Created by Jordi on 5-4-2016.
   */
 class LogReceiverSpec extends SpecBase with ScalatestRouteTest {
-
+  "The log receiver" must {
+    "handle a GET on /components is successfull" in new LogReceiver {
+      Get("/components") ~> routes ~> check {
+        status.isSuccess() shouldEqual true
+      }
+    }
+    "handle a GET on /components response with status code 200" in new LogReceiver {
+      Get("/components") ~> routes ~> check {
+        status.intValue() shouldEqual 200
+      }
+    }
+    "handle a GET on /components response with status code 405" in new LogReceiver {
+     Get("/") ~> routes ~> check {
+        status.intValue() shouldEqual 405
+      }
+    }
+  }
 }
