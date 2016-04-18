e48055f3e3a489a03293385b9d6ae3f2fd393ddc
Ger van Rossum <gvr@users.noreply.github.com>
2016-04-18 22:58:43 +0200

    add log parser actor unit tests

---
+++ src/test/scala/performanceanalysis/administrator/LogParserActorSpec.scala
@@ -0,0 +1,31 @@
+package performanceanalysis.administrator
+
+import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+import performanceanalysis.base.ActorSpecBase
+import performanceanalysis.server.Protocol.{Details, Metric, MetricRegistered, RequestDetails}
+
+class LogParserActorSpec(_system: ActorSystem) extends ActorSpecBase(_system) {
+
+  def this() = this(ActorSystem("AdministratorActorSpec"))
+
+  "LogParserActor" must {
+
+    "send metrics on request for details" in {
+      val logParserActorRef = TestActorRef(new LogParserActor())
+      logParserActorRef ! RequestDetails
+      expectMsg(Details(Nil))
+    }
+
+    "add a metric to it's state" in {
+      val logParserActorRef = TestActorRef(new LogParserActor())
+      val metric = Metric("key", "some regex")
+      logParserActorRef ! metric
+      expectMsg(MetricRegistered(metric))
+      logParserActorRef ! RequestDetails
+      expectMsg(Details(List(metric)))
+    }
+
+  }
+
+}
