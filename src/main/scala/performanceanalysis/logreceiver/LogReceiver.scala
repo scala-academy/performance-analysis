c541752b03f7acefed48e4cebfb36e278f046d56
mihaelaoprea <mikioprea@yahoo.com>
2016-04-19 22:37:36 +0200

    handle posting of a log via LogReceiver

--- src/main/scala/performanceanalysis/logreceiver/LogReceiver.scala
+++ src/main/scala/performanceanalysis/logreceiver/LogReceiver.scala
@@ -1,8 +1,14 @@
 package performanceanalysis.logreceiver
 
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.pattern.ask
+import performanceanalysis.server.Protocol._
 import performanceanalysis.server.Server
+
+import scala.concurrent.Future
 
 /**
   * Created by Jordi on 13-3-2016.
@@ -19,8 +25,25 @@ class LogReceiver extends Server  {
     get {
       log.debug("get /components executed")
       complete("dummy response")
+    } ~
+    pathPrefix(Segment) { componentId =>
+      path("logs") {
+        post {
+          entity(as[String]) { log =>
+            complete(handlePostLog(logReceiverActor ? SubmitLogs(componentId, log)))
+          }
+        }
+      }
     }
   }
 
   def routes: Route = componentsRoute
+
+  private def handlePostLog(resultFuture: Future[Any]): Future[HttpResponse] = {
+    resultFuture.flatMap {
+      case LogParserNotFound(componentId) =>
+        Future(HttpResponse(status = NotFound))
+      case "OK" => Future(HttpResponse(status = OK))
+    }
+  }
 }
