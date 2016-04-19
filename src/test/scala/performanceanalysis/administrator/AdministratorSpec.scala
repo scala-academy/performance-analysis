8d93a6ea62b4a0b5bf151c3d4587550d8e24e30a
Ger van Rossum <gvr@users.noreply.github.com>
2016-04-19 19:20:32 +0200

    corrected after merge with feature 10

--- src/test/scala/performanceanalysis/administrator/AdministratorSpec.scala
+++ src/test/scala/performanceanalysis/administrator/AdministratorSpec.scala
@@ -1,9 +1,8 @@
 package performanceanalysis.administrator
 
-import akka.actor.ActorRef
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.testkit.{TestActor, TestProbe}
+import akka.testkit.TestProbe
 import performanceanalysis.base.SpecBase
 import performanceanalysis.server.Protocol._
 
@@ -14,63 +13,75 @@ class AdministratorSpec extends SpecBase with ScalatestRouteTest {
 
   "The server" must {
     "create an administrator actor and route messages to it" in new Administrator(system.deadLetters) {
-      val probe = TestProbe("AdministratorActorProbe")
-      probe.setAutoPilot(new TestActor.AutoPilot {
-        def run(sender: ActorRef, msg: Any): TestActor.AutoPilot = {
-          sender ! RegisteredComponents(Set())
-          TestActor.NoAutoPilot
-        }
-      })
+      val probe = TestProbe()
       override protected val administratorActor = probe.ref
-      Get("/components") ~> routes ~> check {
-        probe.expectMsg(GetRegisteredComponents)
+
+      val routeTestResult = Get("/components") ~> routes
+
+      probe.expectMsg(GetRegisteredComponents)
+      probe.reply(RegisteredComponents(Set()))
+
+      routeTestResult ~> check {
+        status shouldBe StatusCodes.OK
+        responseAs[RegisteredComponents] shouldBe RegisteredComponents(Set())
       }
     }
 
     "handle a GET on /components by returning all registered componentIds" in new Administrator(system.deadLetters) {
-      val probe = TestProbe("AdministratorActorProbe")
-      probe.setAutoPilot(new TestActor.AutoPilot {
-        def run(sender: ActorRef, msg: Any): TestActor.AutoPilot = {
-          sender ! RegisteredComponents(Set("RegisteredComponent1", "RegisteredComponent2", "RegisteredComponent3"))
-          TestActor.NoAutoPilot
-        }
-      })
+      val probe = TestProbe()
       override protected val administratorActor = probe.ref
-      Get("/components") ~> routes ~> check {
-        val result = responseAs[RegisteredComponents]
-        result shouldBe RegisteredComponents(Set("RegisteredComponent1", "RegisteredComponent2", "RegisteredComponent3"))
+
+      val testRouteResult = Get("/components") ~> routes
+
+      probe.expectMsg(GetRegisteredComponents)
+      probe.reply(RegisteredComponents(Set("RegisteredComponent1", "RegisteredComponent2", "RegisteredComponent3")))
+
+      testRouteResult ~> check {
+        responseAs[RegisteredComponents] shouldBe
+          RegisteredComponents(Set("RegisteredComponent1", "RegisteredComponent2", "RegisteredComponent3"))
       }
     }
 
     "handle a GET on /components/<known componentId> with details of that component" in new Administrator(system.deadLetters) {
-      val probe = TestProbe("AdministratorActorProbe")
-      val componentId = "knownId"
-      probe.setAutoPilot(new TestActor.AutoPilot {
-        def run(sender: ActorRef, msg: Any): TestActor.AutoPilot = {
-          sender ! Details(Nil)
-          TestActor.NoAutoPilot
-        }
-      })
+      val probe = TestProbe()
       override protected val administratorActor = probe.ref
 
-      Get(s"/components/$componentId") ~> routes ~> check {
-        probe.expectMsgPF() {case GetDetails(`componentId`) => true}
-        val result = responseAs[Details]
-        result shouldBe Details(Nil)
+      val componentId = "knownId"
+      val routeTestResult = Get(s"/components/$componentId") ~> routes
+
+      probe.expectMsgPF() { case GetDetails(`componentId`) => true }
+      probe.reply(Details(Nil))
+
+      routeTestResult ~> check {
+        responseAs[Details] shouldBe Details(Nil)
+      }
+    }
+
+    "handle a GET on /components/<known componentId>/metrics with details of that component" in new Administrator(system.deadLetters) {
+      val probe = TestProbe()
+      override protected val administratorActor = probe.ref
+
+      val componentId = "knownId"
+      val routeTestResult = Get(s"/components/$componentId/metrics") ~> routes
+
+      probe.expectMsgPF() { case GetDetails(`componentId`) => true }
+      probe.reply(Details(Nil))
+
+      routeTestResult ~> check {
+        responseAs[Details] shouldBe Details(Nil)
       }
     }
 
     "handle a POST on /components by creating a new registered componentId" in new Administrator(system.deadLetters) {
-      val probe = TestProbe("AdministratorActorProbe")
-      probe.setAutoPilot(new TestActor.AutoPilot {
-        def run(sender: ActorRef, msg: Any): TestActor.AutoPilot = {
-          sender ! LogParserCreated("RegisteredComponent1")
-          TestActor.NoAutoPilot
-        }
-      })
+      val probe = TestProbe()
       override protected val administratorActor = probe.ref
 
-      Post("/components", RegisterComponent("RegisteredComponent1")) ~> routes ~> check {
+      val routeTestResult = Post("/components", RegisterComponent("RegisteredComponent1")) ~> routes
+
+      probe.expectMsg(RegisterComponent("RegisteredComponent1"))
+      probe.reply(LogParserCreated("RegisteredComponent1"))
+
+      routeTestResult ~> check {
         response.status shouldBe StatusCodes.Created
       }
     }
