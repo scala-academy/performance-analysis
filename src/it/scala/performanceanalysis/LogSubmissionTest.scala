a8ac91f874bd1ea96f3ccb56c56e9627a00db628
Ruud Prein <ruud.prein@ing.nl>
2016-04-20 18:49:31 +0200

    setup integration test senario

--- src/it/scala/performanceanalysis/LogSubmissionTest.scala
+++ src/it/scala/performanceanalysis/LogSubmissionTest.scala
@@ -2,8 +2,10 @@ package performanceanalysis
 
 import performanceanalysis.base.IntegrationTestBase
 import com.twitter.finagle.http
+import com.twitter.finagle.http.Response
 import org.scalatest.concurrent.ScalaFutures
 import performanceanalysis.utils.TwitterFutures
+
 import scala.concurrent.duration._
 
 class LogSubmissionTest extends IntegrationTestBase with ScalaFutures with TwitterFutures {
@@ -20,12 +22,31 @@ class LogSubmissionTest extends IntegrationTestBase with ScalaFutures with Twitt
       }
     }
     scenario("Logs posted at the LogReceiver") {
-      // TODO Implement
       Given("the server is running")
-      And("""I registered a component with id "parsingConfiguredComponenet" and a metric {"regex" : "+d", "metric-key" : "a-numerical-metric"}""")
-      When("""I do a POST with {"logline" : "some action took 101 seconds", "metric-key" : "a-numerical-metric"} to /components/parsConfigComp/logs on the """ +
+
+      And("""I registered a component with id "parsingConfiguredComponent"""")
+      registerComponent("parsingConfiguredComponent")
+
+      And("""And a metric {"regex" : "+d", "metric-key" : "a-numerical-metric"} to /components/parsingConfiguredComponent on the Administrator port""")
+      val path = "/components/parsingConfiguredComponent"
+      val data = """{"regex" : "+d", "metric-key" : "a-numerical-metric"}"""
+      awaitAdminPostResonse(path, data)
+
+      When("""I do a POST with {"logline" : "some action took 101 seconds", "metric-key" : "a-numerical-metric"} to /components/parsingConfiguredComponent/logs on the """ +
           "LogReceiver port")
-      Then("""the response should have statuscode 202""")
+      // TODO post
+
+      whenReady(response, timeout(1.seconds)) { result =>
+        assert(result.getStatusCode() === 202)
+        Then("""the response should have statuscode 202""")
+      }
     }
   }
+
+  def registerComponent(componentId: String): Response = {
+    val path = "/components"
+    val data = s"""{"componentId" : "${componentId}"}"""
+    awaitAdminPostResonse(path, data)
+  }
+
 }
