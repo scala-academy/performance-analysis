412e3bfed371963756ab9ca9c8bac56163a8df31
Ger van Rossum <gvr@users.noreply.github.com>
2016-05-20 23:39:46 +0200

    less lines per method

--- src/main/scala/performanceanalysis/DateParser.scala
+++ src/main/scala/performanceanalysis/DateParser.scala
@@ -11,17 +11,7 @@ object DateParser {
 
   private val regex = """(\d{1,2}|\d{4})[ -/\.](\d{1,2})[ -/\.](\d{1,2}|\d{4})[ T]( \d|\d{2})[:.](\d{2})[.:](\d{2})(\.\d{0,9})?""".r
 
-  private def parseNano(s: String): Int = {
-    // note that an optional group will be null when not supplied
-    Try(Option(s)).toOption.flatten match {
-      case None => 0
-      case Some(".") => 0
-      case Some(s) => (s.toDouble * 1000000000).toInt
-    }
-  }
-
   private def parser(iYear: Int, iMonth: Int, iDay: Int): DateParser = new DateParser(iYear, iMonth, iDay)
-
 
   val parseYMD: (String) => Option[LocalDateTime] = parser(1, 2, 3).parser
 
@@ -29,20 +19,30 @@ object DateParser {
 
   val parseMDY: (String) => Option[LocalDateTime] = parser(3, 1, 2).parser
 
-
 }
 
 private class DateParser(iYear: Int, iMonth: Int, iDay: Int) {
 
-  private def rawParser(m: Match): LocalDateTime = {
-    val year = m.group(iYear).toInt
-    val month = m.group(iMonth).toInt
-    val day = m.group(iDay).toInt
-    val hour = m.group(4).toInt
-    val min = m.group(5).toInt
-    val sec = m.group(6).toInt
-    val nano = parseNano(m.group(7))
-    LocalDateTime.of(year, month, day, hour, min, sec, nano)
+  def year(m: Match) = m.group(iYear).toInt
+
+  def month(m: Match) = m.group(iMonth).toInt
+
+  def day(m: Match) = m.group(iDay).toInt
+
+  def hour(m: Match) = m.group(4).toInt
+
+  def min(m: Match) = m.group(5).toInt
+
+  def sec(m: Match) = m.group(6).toInt
+
+  def nano(m: Match) = Try(Option(m.group(7))).toOption.flatten match {
+    case None => 0
+    case Some(".") => 0
+    case Some(s) => (s.toDouble * 1000000000).toInt
+  }
+
+  def rawParser(m: Match): LocalDateTime = {
+    LocalDateTime.of(year(m), month(m), day(m), hour(m), min(m), sec(m), nano(m))
   }
 
   def parser(s: String): Option[LocalDateTime] =
