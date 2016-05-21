daef52a4010526a984722659e8b80871f69a30d3
Ger van Rossum <gvr@users.noreply.github.com>
2016-05-21 14:21:45 +0200

    simplified timeparser

--- src/main/scala/performanceanalysis/TimeParser.scala
+++ src/main/scala/performanceanalysis/TimeParser.scala
@@ -7,19 +7,24 @@ import scala.util.matching.Regex.Match
 
 class TimeParser(shift: Int) {
 
-  val iHour = shift + 1
+  private val iHour = shift + 1
 
-  val iMin = shift + 2
+  private val iMin = shift + 2
 
-  val iSec = shift + 3
+  private val iSec = shift + 3
 
-  val iNano = iSec + 1
+  private val iNano = iSec + 1
+
+  private def nanoOption(s: String): Option[String] = {
+    val nanoTry = Try(Option(s))
+    nanoTry.toOption.flatten
+  }
 
   def parse(m: Match): LocalTime = {
     val hour = m.group(iHour).toInt
     val min = m.group(iMin).toInt
     val sec = m.group(iSec).toInt
-    val nano = Try(Option(m.group(iNano))).toOption.flatten match {
+    val nano = nanoOption(m.group(iNano)) match {
       case None => 0
       case Some(".") => 0
       case Some(s) => (s.toDouble * 1000000000).toInt
